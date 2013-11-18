d9c1580f9b1ffafd25328ee346b11539726dcfcd
zgw <645822731@qq.com>
2013-11-18 17:50:31 +0800

    Initial Commit

---
+++ PopButtonsCompent/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PopButtonsCompent
+//
+//  Created by zgw on 13-11-18.
+//  Copyright (c) 2013年 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PopButtonsCompent/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PopButtonsCompent
+//
+//  Created by zgw on 13-11-18.
+//  Copyright (c) 2013年 zhao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
